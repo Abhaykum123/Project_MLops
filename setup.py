2157d56e65b48150a0fbbf4b495b30260c6da849
Abhaykum123 <anita824122@gmail.com>
2025-11-09 01:32:44 +0530

    first code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Abhay kumar',
+    author_email='abhaykumargupta9939@gmail.com',
+    package=find_packages()
+)
